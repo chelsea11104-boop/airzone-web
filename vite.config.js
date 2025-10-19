80c64eee02733b8b2d597679184b2324452629f6
chelsea11104-boop <chelsea11104@gmail.com>
2025-10-19 16:20:24 +0700

    update build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Ganti "airzone-web" dengan nama repository GitHub kamu persis
 export default defineConfig({
   plugins: [react()],
+  base: '/airzone-web/', 
 })

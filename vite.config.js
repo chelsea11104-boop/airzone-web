23b10f66748e0fb78f54dcdcab22b5ebb99f0b12
chelsea11104-boop <chelsea11104@gmail.com>
2025-11-12 15:35:53 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Ganti "airzone-web" dengan nama repository GitHub kamu persis
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: '/', 
 })
